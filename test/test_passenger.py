99fe6104089c5bf2a3003fd5dc77d575918fe564
gusnorge <gustavolima1703@gmail.com>
2026-03-23 03:52:38 -0300

    adicionado testes restantes do módulo passenger e configurado monkey patch para time.sleep

--- test/test_passenger.py
+++ test/test_passenger.py
@@ -12,11 +12,8 @@ import src.passenger
     (6)
     ])
 def test_get_passenger_destination_not_0_in_ground_floor(
-    mocker,
     new_pass
 ):
-    mock_sleep = mocker.patch('src.passenger.sleep')
-    mock_sleep.return_value = None
     current_floor = 0
     new_passenger = new_pass
 
@@ -39,8 +36,6 @@ def test_get_passenger_destination_not_0_in_ground_floor(
 def test_get_passenger_destination_0_not_ground_floor_chance_false(
     mocker,new_pass
 ):
-    mock_sleep = mocker.patch('src.passenger.sleep')
-    mock_sleep.return_value = None
     mock_chance_happens = mocker.patch('src.passenger.happens_by_chance')
     mock_chance_happens.return_value = False
     current_floor = 15
@@ -65,13 +60,11 @@ def test_get_passenger_destination_0_not_ground_floor_chance_false(
 def test_get_passenger_destination_not_0_not_ground_floor_chance_true(
     mocker,new_pass
 ):
-    mock_sleep = mocker.patch('src.passenger.sleep')
     mock_chance_happens = mocker.patch('src.passenger.happens_by_chance')
     mock_randint = mocker.patch('src.passenger.randint')
     current_floor = 15
     new_passenger = new_pass
 
-    mock_sleep.return_value = None
     mock_chance_happens.return_value = True
     mock_randint.return_value = 6
 
@@ -83,3 +76,98 @@ def test_get_passenger_destination_not_0_not_ground_floor_chance_true(
     print(new_destination)
 
     assert len(new_destination) and not new_destination.count(0)
+
+
+def test_board_passanger_lift_capacity_exceeded(mocker):
+    current_floor = 1 
+    floor_passenger = {1:4}
+    passenger_aboard = 3
+    old_passenger_aboard = passenger_aboard
+    floor_queue = [1]
+    destination = [4,7,3]
+    old_destination = destination.copy()
+    mock_chance = mocker.patch('src.passenger.happens_by_chance')
+    mock_chance.return_value = True
+
+    result_board_passanger = src.passenger.board_passenger(
+        current_floor=current_floor,
+        floor_passenger=floor_passenger,
+        passenger_aboard=passenger_aboard,
+        floor_queue=floor_queue,
+        destination=destination
+    )
+
+    updated_passenger_aboard = result_board_passanger['updated_passenger_aboard']
+    updated_destination = result_board_passanger['updated_destination']
+
+    assert old_destination != updated_destination and old_passenger_aboard != updated_passenger_aboard 
+
+
+def test_board_passanger_lift_capacity_not_exceeded(mocker):
+    current_floor = 1 
+    floor_passenger = {1:3}
+    old_floor_passenger = floor_passenger.copy()
+    passenger_aboard = 3
+    old_passenger_aboard = passenger_aboard
+    floor_queue = [1]
+    destination = [4,7,3]
+    old_destination = destination.copy()
+    mock_chance = mocker.patch('src.passenger.happens_by_chance')
+    mock_chance.return_value = True
+
+    result_board_passenger = src.passenger.board_passenger(
+        current_floor=current_floor,
+        floor_passenger=floor_passenger,
+        passenger_aboard=passenger_aboard,
+        floor_queue=floor_queue,
+        destination=destination
+    )
+
+    updated_passenger_aboard = result_board_passenger['updated_passenger_aboard']
+    updated_destination = result_board_passenger['updated_destination']
+    updated_floor_passenger = result_board_passenger['updated_floor_passenger']
+
+    assert old_destination != updated_destination 
+    assert old_passenger_aboard != updated_passenger_aboard
+    assert old_floor_passenger !=updated_floor_passenger
+
+
+def test_unboard_passenger_with_passenger_to_unboard():
+    passenger_aboard = 3
+    old_passenger_aboard = passenger_aboard
+    current_floor = 5
+    destination = [5,4,3]
+    old_destination = destination.copy()
+
+    result_unboard_passenger = src.passenger.unboard_passenger(
+        passenger_aboard=passenger_aboard,
+        current_floor=current_floor,
+        destination=destination
+    )
+
+    updated_passenger_aboard = result_unboard_passenger['updated_passenger_aboard']
+    updated_destination = result_unboard_passenger['updated_destination']
+
+    assert old_passenger_aboard != updated_passenger_aboard
+    assert old_destination != updated_destination
+
+
+def test_unboard_passenger_without_passenger_to_unboard():
+    passenger_aboard = 3
+    old_passenger_aboard = passenger_aboard
+    current_floor = 6
+    destination = [5,4,3]
+    old_destination = destination.copy()
+
+    result_unboard_passenger = src.passenger.unboard_passenger(
+        passenger_aboard=passenger_aboard,
+        current_floor=current_floor,
+        destination=destination
+    )
+
+    updated_passenger_aboard = result_unboard_passenger['updated_passenger_aboard']
+    updated_destination = result_unboard_passenger['updated_destination']
+
+    assert old_passenger_aboard == updated_passenger_aboard
+    assert old_destination == updated_destination
+
